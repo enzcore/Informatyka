569b2240f422a154763e265ba5765991fcfc062f
enzcore <148456572+enzcore@users.noreply.github.com>
2026-04-14 08:21:54 +0200

    Update Przedzial.py

--- Przedzial.py
+++ Przedzial.py
@@ -2,32 +2,23 @@ def f(x):
     return x*(x*(x-3)+2)-6
 
 
-def polowienie_przedzialow(a, b, epsilon):
-
-    if f(a) == 0.0:
-        return a
-
-    if f(b) == 0.0:
-        return b
-
+def bisekcja(a, b, epsilon):
     while (b - a) > epsilon:
-        srodek = (a + b) / 2
+        s = (a + b) / 2
 
-        if f(srodek) == 0:
-            return srodek
-
-        if f(a) * f(srodek) < 0:
-            b = srodek
+        if f(a) * f(s) < 0:
+            b = s
         else:
-            a = srodek
+            a = s
 
     return (a + b) / 2
 
 
-a = -10
-b = 10
+a = float(input("Podaj a: "))
+b = float(input("Podaj b: "))
+
 epsilon = 0.00001
 
-wynik = polowienie_przedzialow(a, b, epsilon)
+wynik = bisekcja(a, b, epsilon)
 
-print("Znalezione miejsce zerowe wynosi:", format(wynik, ".5f"))
+print("Miejsce zerowe:", format(wynik, ".5f"))
